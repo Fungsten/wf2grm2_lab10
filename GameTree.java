3fe8508d392390793815770803bac1ba713f49b8
William Fung <19wf2@tcl217a-24.cs.williams.edu>
2017-05-03 16:10:29 -0400

    BOOOOOOOOOOOOOOOOO

--- GameTree.java
+++ GameTree.java
@@ -13,63 +13,79 @@ class GameTree{
   public final static char WHITE = 'o';
   public final static char BLACK = '*';
   int nodenumber = 0;
+  int counter = 0;
+
 
   GameTree(){
-    this.groot = new GameNode(false, this.b, null, this.color);
+    this.groot = new GameNode(false, this.b, null, BLACK);
   }
 
+
   public void addNodes(GameNode parent, char color){
-    System.out.println("this.b.moves(color).size(): " + parent.currBoard.moves(color).size());
+    //System.out.println("this.b.moves(color).size(): " + parent.currBoard.moves(color).size());
     GameNode gn;
 
   	Vector<HexMove> moves;
     HexBoard newB;
-  	int move = 105;
+  	int move = 0;
 
     // move to play
     moves = parent.currBoard.moves(color);
-    System.out.println(moves.size());
+    //System.out.println(moves.size());
+    nodenumber = nodenumber + moves.size();
     Iterator iter = moves.iterator();
     while (iter.hasNext()){
 
       // read move
       newB = new HexBoard(parent.currBoard, moves.elementAt(move));
       gn = new GameNode(newB.win(color), newB, moves.elementAt(move), color);
-      System.out.println(gn.toString());
+      //System.out.println(gn.toString());
       parent.addMove(gn);
-      nodenumber++;
       iter.next();
       move++;
     }
+    /*
     for (int i = 0; i < parent.possMoves.size(); i++){
-      int counter = 0;
-      if (parent.isWin == true){
-        // pass
+
+      if (counter % 2 == 0){
+        addNodes(parent.possMoves.elementAt(i), HexBoard.WHITE);
       } else {
-        if (counter % 2 == 0){
+        addNodes(parent.possMoves.elementAt(i), HexBoard.BLACK);
+      }
+
+      if (parent.isWin == true){
+      } else {
+        if (parent.color == HexBoard.BLACK){
           addNodes(parent.possMoves.elementAt(i), HexBoard.WHITE);
-          counter++;
         } else {
           addNodes(parent.possMoves.elementAt(i), HexBoard.BLACK);
-          counter++;
+        }
+      }
+    }
+    */
+  }
+
+
+  public void populate(GameNode parent){
+    if (parent.possMoves.size() == 0 || parent.isWin == true){
+      System.out.println("Winner winner chicken dinner");
+    } else {
+      for (int i = 0; i < parent.possMoves.size(); i++){
+        System.out.println("WEE WOO WEE WOO");
+        if (parent.color == HexBoard.BLACK){
+          //System.out.println("I passed! tutrle");
+          addNodes(parent, HexBoard.WHITE);
+          //System.out.println("I passed! poato");
+          populate(parent.possMoves.elementAt(i));
+          //System.out.println("I passed! tornato");
+        } else {
+          //System.out.println("I passed! black");
+          addNodes(parent, HexBoard.BLACK);
+          populate(parent.possMoves.elementAt(i));
         }
       }
     }
   }
-
-  /*public void populate(GameNode parent){
-    int counter = 0;
-    if (parent.isWin == true){
-      // pass
-    } else {
-      if (counter % 2 == 0){
-        addNodes(parent, HexBoard.WHITE);
-      } else {
-        addNodes(parent, HexBoard.BLACK);
-      }
-
-    }
-  }*/
 
 
 
@@ -80,7 +96,10 @@ class GameTree{
   public static void main(String[] args){
     System.out.println("I compiled!");
     GameTree babygroot = new GameTree();
-    babygroot.addNodes(babygroot.groot, WHITE);
+    babygroot.addNodes(babygroot.groot, HexBoard.BLACK);
+    System.out.println(babygroot.groot.toSillyString());
+    System.out.println("I passed!");
+    //babygroot.populate(babygroot.groot);
     System.out.println("node number: " + babygroot.nodenumber);
   }
 
