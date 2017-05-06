37de1d946de2555ca03bc77a3d9d149591ae0697
William Fung <19wf2@tcl217a-24.cs.williams.edu>
2017-05-06 19:49:24 -0400

    cactus

---
+++ GamePlay.java
@@ -0,0 +1,16 @@
+//This is the file you should run to grade our project
+//by the illustrious Grace Mazzarella and the absurd Will Fung (whose last name is not Fury)
+
+//import java.util.*;
+import structure5.*;
+
+public class GamePlay{
+
+  GameTree babygroot = new GameTree();
+
+
+  public GamePlay(Player playerOne, Player playerTwo){
+    
+  }
+
+}
