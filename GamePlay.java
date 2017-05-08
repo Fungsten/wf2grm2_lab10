a589250cf0c936f506fd3befb3658f80648ef1f7
i-am-a-turtle <grm2@williams.edu>
2017-05-07 21:05:34 -0400

    moving is happening

--- GamePlay.java
+++ GamePlay.java
@@ -7,30 +7,30 @@ import structure5.*;
 public class GamePlay{
 
   GameTree root;
-  GameNode currNode;
   Player p1;
   Player p2;
   int p1Wins;
   int p2Wins;
 
 
-  public GamePlay(String playerOne, String playerTwo){
+  public GamePlay(String playerOne, String colorOne, String playerTwo, String colorTwo){
     this.root = new GameTree();
-    this.currNode = root.groot;
     if (playerOne.equals("h")){
-      this.p1 = new Player(playerOne, HexBoard.WHITE);
+      this.p1 = new Player(playerOne);
     } else if (playerOne.equals("r")){
-      this.p1 = new Player(playerOne, HexBoard.WHITE);
+      this.p1 = new Player(playerOne);
     } else {
-      this.p1 = new Player(playerOne, HexBoard.WHITE);
+      this.p1 = new Player(playerOne);
     }
+    this.p1.setColor(colorOne);
     if (playerTwo.equals("h")){
-      this.p2 = new Player(playerTwo, HexBoard.BLACK);
+      this.p2 = new Player(playerTwo);
     } else if (playerTwo.equals("r")){
-      this.p2 = new Player(playerTwo, HexBoard.BLACK);
+      this.p2 = new Player(playerTwo);
     } else {
-      this.p2 = new Player(playerTwo, HexBoard.BLACK);
+      this.p2 = new Player(playerTwo);
     }
+    this.p2.setColor(colorTwo);
     this.p1Wins = 0;
     this.p2Wins = 0;
   }
@@ -47,15 +47,50 @@ public class GamePlay{
     Player currPlayer = this.p1;
     GameNode playedNode = this.root.groot;
 
+    // play two moves, remove the null state of the two players current nodes
+    // we can't set the two players current nodes to the root or else the while loop won't execute
+    // P1 plays once
+    currPlayer.play(playedNode, oppPlayer(currPlayer));
+    playedNode = currPlayer.currNode;
+    currPlayer = this.oppPlayer(currPlayer);
+    // P2 plays once
+    currPlayer.play(playedNode, oppPlayer(currPlayer));
+    playedNode = currPlayer.currNode;
+    currPlayer = this.oppPlayer(currPlayer);
+
+    // Play continues until there is a winner
     while (!currPlayer.currNode.equals(playedNode)){
       currPlayer.play(playedNode, oppPlayer(currPlayer));
-      currPlayer = oppPlayer(currPlayer);
       playedNode = currPlayer.currNode;
+      currPlayer = this.oppPlayer(currPlayer);
     }
     if (currPlayer.equals(this.p1)){
       ++this.p1Wins;
     } else {
       ++this.p2Wins;
+    }
+    // Ask to play again
+    if (this.p1.type.equals("human") || this.p2.type.equals("human")){
+      Scanner in = new Scanner(System.in);
+      System.out.println("Would the human(s) like to play again?");
+      System.out.println("Type 'y' for yes and 'n' for no.");
+      String answer = in.nextLine();
+      if (answer.equals("y")){
+        this.go();
+      } else if (answer.equals("n")){
+        return;
+      } else {
+        System.out.println("You did not input a valid answer. If you input a second invalid answer, you will quit and lose all progress.");
+        String answer2 = in.nextLine();
+        if (answer2.equals("y")){
+          this.go();
+        } else if (answer2.equals("n")){
+          return;
+        } else {
+          System.out.println("You have quit");
+          return;
+        }
+      }
     }
   }
 
@@ -63,10 +98,15 @@ public class GamePlay{
     /*Scanner in = new Scanner(System.in);
     System.out.println("Player 1: please input 'h' to play as yourself, 'r' to watch a random player play, or 'c' to watch a learning computer play.");
     String arg1 = in.next();
+    System.out.println("Player 1: please input 'w' to play as white and go first or 'b' to play as black and go second.")
+    String arg2 = in.next();
     System.out.println("Player 2: please input 'h' to play as yourself, 'r' to watch a random player play, or 'c' to watch a learning computer play.");
-    String arg2 = in.next();*/
-    GamePlay hexapawn = new GamePlay("h", "r");
-    //System.out.println(hexapawn.currNode.children.size());
+    String arg3 = in.next();
+    System.out.println("Player 2: please input 'w' to play as white and go first or 'b' to play as black and go second.")
+    String arg4 = in.next();*/
+    GamePlay hexapawn = new GamePlay("h", "w", "r", "b");
+    //System.out.println(hexapawn.oppPlayer(hexapawn.p1).type);
+    //System.out.println(hexapawn.p2.type);
     hexapawn.go();
     System.out.println("P1 wins: " + hexapawn.p1Wins);
     System.out.println("P2 wins: " + hexapawn.p2Wins);
--- Player.java
+++ Player.java
@@ -11,8 +11,8 @@ public class Player {
   protected GameNode currNode;
   //protected GameTree gametree;
 
-  public Player(String type, char color){
-    this.color = color;
+  public Player(String type){
+    this.color = HexBoard.WHITE; // a default that is changed in GamePlay.java
     if (type.equals("h")){
       this.type = "human";
     } else if (type.equals("r")){
@@ -26,13 +26,24 @@ public class Player {
     this.currNode = null;
   }
 
+  public void setColor(String color){
+    if (color.equals("b")){
+      this.color = HexBoard.BLACK;
+    } else if (color.equals("w")){
+      this.color = HexBoard.WHITE;
+    } else {
+      System.out.println("You input an invalid choice and have automatically been assigned to white.");
+      this.color = HexBoard.WHITE;
+    }
+  }
+
   // Pre: takes a non-null GameNode and a valid player
   // Post: if no one has won, returns a GameNode based on the chosen move
   //       if someone has won, returns the input GameNode
   public GameNode play(GameNode node, Player opponent){
     HexBoard board = node.currBoard;
     Vector<GameNode> moves = node.children;
-    GameNode result = node;
+    GameNode newNode;
     System.out.println(node.toSillyString());
 
 
@@ -40,21 +51,18 @@ public class Player {
     if (this.type.equals("human")){
       Scanner in = new Scanner(System.in);
       System.out.println("Please type one of the given integers: ");
-      int hMove = in.nextInt() + 1;
+      int hMove = in.nextInt() - 1;
       if (hMove > moves.size()){
         System.out.println("You have input an invalid move. If you choose to type an invalid move again, you will forfeit the game.");
         hMove = in.nextInt() + 1;
         if (hMove > moves.size()){
           //result = node;
           System.out.println("You're not very good at this, are you, mate?");
-          return result;
+          return node;
         } else {
           board = new HexBoard(board, moves.elementAt(hMove).hm);
         }
       }
-      System.out.println("This is the current board:");
-      System.out.println(board.toString());
-
       newNode = new GameNode(board.win(color), board, opponent.color, node, moves.elementAt(hMove).hm);
     }
 
@@ -65,9 +73,6 @@ public class Player {
       int rpMove = rand.nextInt(moves.size());
 
       board = new HexBoard(board, moves.elementAt(rpMove).hm);
-      System.out.println("This is the current board:");
-      System.out.println(board.toString());
-
       newNode = new GameNode(board.win(color), board, opponent.color, node, moves.elementAt(rpMove).hm);
     }
 
@@ -78,35 +83,31 @@ public class Player {
       int aiMove = rand.nextInt(moves.size());
 
       board = new HexBoard(board, moves.elementAt(aiMove).hm);
-      System.out.println("This is the current board:");
-      System.out.println(board.toString());
-
       newNode = new GameNode(board.win(color), board, opponent.color, node, moves.elementAt(aiMove).hm);
     }
 
 
     if (newNode.currBoard.win(this.color)){
+      // If there's a win, return a silly String and outside all of this return the current node
       if (this.type.equals("human")){
-        // If HumanPlayer wins, return corresponding node
         System.out.println("The human actually triumphs!");
-        //result = node;
-
+        return this.currNode;
       } else if (this.type.equals("random")){
-        // If randomPlayer wins, return corresponding node
         System.out.println("A monkey on a typewriter wins! Seriously?");
-        //result = node;
-
-      } else if (this.type.equals("AI")){
-        // If our new robot overlord wins, return corresponding node
-        System.out.println("Our new robot overlord triumphs! You have doomed us all.");
-        //result = node;
-
+        return this.currNode;
       } else {
-        // If nobody won, return the next node;
-        result = newNode;
+        System.out.println("Our new robot overlord triumphs! You have doomed us all.");
+        return this.currNode;
       }
+    } else {
+      // If nobody won, update the current node and return it
+      System.out.println("No one won");
+      for (int i = 0; i < node.children.size(); ++i){
+        if (newNode.hm.equals(node.children.elementAt(i).hm)){
+          this.currNode = node.children.elementAt(i);
+        }
+      }
+      return this.currNode;
     }
-    this.currNode = result;
-    return result;
   }
 }
